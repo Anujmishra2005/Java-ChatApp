5a1b20ed39703e462b7e228434ee6e4c0b752ab4
Anuj Mishra <anujmishraan2005@gmail.com>
2025-06-19 23:55:42 +0530

    Update ChatClientGUI.java

--- ChatClientGUI.java
+++ ChatClientGUI.java
@@ -27,6 +27,7 @@ public class ChatClientGUI {
         JScrollPane scrollPane = new JScrollPane(messageArea);
 
         inputField = new JTextField();
+        sendButton = new JButton("Send");
         
 
         JPanel bottomPanel = new JPanel(new BorderLayout());
